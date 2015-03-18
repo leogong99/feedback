3ab8878923a80dc6e25b3cd4ae832e4405bd4761
Gong, Jing <jingong@amazon.com>
2015-03-18 16:46:25 -0700

    add html2canvas

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,4 +1,4 @@
-define(["jquery", "react"], function($, React) {
+define(["jquery", "react", "html2canvas"], function($, React, html2canvas) {
     //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
    // tutorial1-raw.js
 	var CommentBox = React.createClass({displayName: 'CommentBox',
