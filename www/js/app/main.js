4582502d6acd0fd83255093f7ce5b9063223ca59
Gong, Jing <jingong@amazon.com>
2015-03-17 18:19:11 -0700

    First commit.

---
+++ www/js/app/main.js
@@ -0,0 +1,17 @@
+define(["jquery", "react"], function($, React) {
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+   // tutorial1-raw.js
+	var CommentBox = React.createClass({displayName: 'CommentBox',
+	  render: function() {
+	    return (
+	      React.createElement('div', {className: "commentBox"},
+	        "Hello, world! I am a CommentBox."
+	      )
+	    );
+	  }
+	});
+	React.render(
+	  React.createElement(CommentBox, null),
+	  document.getElementsByTagName('body')[0]
+	);
+});
