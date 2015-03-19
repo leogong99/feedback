a49ea31d010047d14de90d995ea19dfb8a360bbd
Gong, Jing <jingong@amazon.com>
2015-03-19 12:11:20 -0700

    remove proxy url

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -7,10 +7,16 @@ javascript: (function() {
   var link  = document.createElement('link');
   link.rel  = 'stylesheet';
   link.type = 'text/css';
-  link.href = 'http://54.68.180.166/www/css/feedback.min.css';
+  link.href = 'http://54.68.180.166/www/css/feedback.css';
   link.media = 'all';
   
   document.getElementsByTagName('head')[0].appendChild(script);
   document.getElementsByTagName('body')[0].appendChild(document.createElement('feedback'));
   document.getElementsByTagName('head')[0].appendChild(link);
+  var link1  = document.createElement('link');
+  link1.rel  = 'stylesheet';
+  link1.type = 'text/css';
+  link1.media = 'all';
+  link1.href = 'http://54.68.180.166/www/css/bootstrap.css';
+  document.getElementsByTagName('head')[0].appendChild(link1);
 })();
