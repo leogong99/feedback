28d7b98d128aa92a58c33226ec8b6093b0855155
Gong, Jing <jingong@amazon.com>
2015-03-18 17:48:53 -0700

    add css to feedback tool

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -6,4 +6,12 @@ javascript: (function() {
     
     document.getElementsByTagName('body')[0].appendChild(document.createElement('feedback'));
     document.getElementsByTagName('head')[0].appendChild(script);
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'http://54.68.180.166/www/css/feedback.min.css';
+    link.media = 'all';
+    head.appendChild(link);
 })();
