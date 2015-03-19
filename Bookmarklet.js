0772a2e6417cb6a53b3d7ed87a9742e2a7761d32
Gong, Jing <jingong@amazon.com>
2015-03-18 18:12:49 -0700

    bookmarklet

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,16 +1,15 @@
 javascript: (function() {
-    var script = document.createElement('script');
-    script.setAttribute('src', 'http://54.68.180.166/www/js/lib/require.js');
-    script.setAttribute('type', 'text/javascript');
-    script.setAttribute('data-main', 'http://54.68.180.166/www/js/app');
-    
-    document.getElementsByTagName('body')[0].appendChild(document.createElement('feedback'));
-    
-    var head  = document.getElementsByTagName('head')[0];
-    var link  = document.createElement('link');
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'http://54.68.180.166/www/css/feedback.min.css';
-    link.media = 'all';
-    document.getElementsByTagName('head')[0].appendChild(script).appendChild(link);
+  var script = document.createElement('script');
+  script.setAttribute('src', 'http://54.68.180.166/www/js/lib/require.js');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('data-main', 'http://54.68.180.166/www/js/app');
+   var head  = document.getElementsByTagName('head')[0];
+  var link  = document.createElement('link');
+  link.rel  = 'stylesheet';
+  link.type = 'text/css';
+  link.href = 'http://54.68.180.166/www/css/feedback.min.css';
+  link.media = 'all';
+  document.getElementsByTagName('head')[0].appendChild(link);
+  document.getElementsByTagName('head')[0].appendChild(script);
+  document.getElementsByTagName('body')[0].appendChild(document.createElement('feedback'));
 })();
