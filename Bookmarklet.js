eedbd1b3c1cde779c34b23e454e3c3d022f02f07
Gong, Jing <jingong@amazon.com>
2015-03-18 18:06:47 -0700

    load css first

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -5,13 +5,12 @@ javascript: (function() {
     script.setAttribute('data-main', 'http://54.68.180.166/www/js/app');
     
     document.getElementsByTagName('body')[0].appendChild(document.createElement('feedback'));
-    document.getElementsByTagName('head')[0].appendChild(script);
+    
     var head  = document.getElementsByTagName('head')[0];
     var link  = document.createElement('link');
-    link.id   = cssId;
     link.rel  = 'stylesheet';
     link.type = 'text/css';
     link.href = 'http://54.68.180.166/www/css/feedback.min.css';
     link.media = 'all';
-    head.appendChild(link);
+    document.getElementsByTagName('head')[0].appendChild(script).appendChild(link);
 })();
