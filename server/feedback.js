12811e4de74188eef465f767d88202786bbf2d37
Gong, Jing <jingong@amazon.com>
2015-03-22 23:38:47 -0700

    express send 200 back

--- server/feedback.js
+++ server/feedback.js
@@ -2,5 +2,6 @@ exports.save = function(req, res) {
 	var comment = req.body.Issues;
 	var img = req.body.data;
 	console.log("User name = " + comment + ", password is " + img);
-	res.end("yes");
+	res.sendStatus(200)
+	res.end("Thanks for your feedback.");
 };
