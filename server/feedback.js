9de3df1f85dc64a296fb8059a76c8dbe83aa42c5
Gong, Jing <jingong@amazon.com>
2015-03-22 23:41:52 -0700

    remove 200 code

--- server/feedback.js
+++ server/feedback.js
@@ -2,6 +2,5 @@ exports.save = function(req, res) {
 	var comment = req.body.Issues;
 	var img = req.body.data;
 	console.log("User name = " + comment + ", password is " + img);
-	res.sendStatus(200)
 	res.end("Thanks for your feedback.");
 };
