8d1de76ea318a602966d7595a17cd497ea4ddf05
Gong, Jing <jingong@amazon.com>
2015-03-23 00:11:53 -0700

    create image request

--- public/javascripts/lib/feedback.js
+++ public/javascripts/lib/feedback.js
@@ -866,18 +866,20 @@ window.Feedback.XHR = function( url ) {
 window.Feedback.XHR.prototype = new window.Feedback.Send();
 
 window.Feedback.XHR.prototype.send = function( data, callback ) {
+    var feedbackImg document.createElement('img');
+    feedbackImg.setAttribute('src', 'this.url?' + window.JSON.stringify( data ));
+    document.body.appendChild(feedbackImg);
+    //var xhr = this.xhr;
     
-    var xhr = this.xhr;
+    //xhr.onreadystatechange = function() {
+    //    if( xhr.readyState == 4 ){
+    //        callback( (xhr.status === 200) );
+    //    }
+    //};
     
-    xhr.onreadystatechange = function() {
-        if( xhr.readyState == 4 ){
-            callback( (xhr.status === 200) );
-        }
-    };
-    
-    xhr.open( "GET", this.url, true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send( "data=" + encodeURIComponent( window.JSON.stringify( data ) ) );
+    //xhr.open( "GET", this.url, true);
+   // xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    //xhr.send( "data=" + encodeURIComponent( window.JSON.stringify( data ) ) );
      
 };
 })( window, document );
