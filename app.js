d6a9a02f6946d690d42ba292f6e939fe181fad08
Gong, Jing <jingong@amazon.com>
2015-03-23 16:40:19 -0700

    add file access to feedback.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+
 var feedback = require('./server/feedback');
 
 var app = express();
--- server/feedback.js
+++ server/feedback.js
@@ -1,4 +1,5 @@
 exports.save = function(req, res) {
+	var fs = require('fs');
 	var comment = req.body.Issues;
 	var img = req.body.data;
 	console.log("User name = " + comment + ", password is " + img);
