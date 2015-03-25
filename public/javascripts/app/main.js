67c2331a1b3061b158bb94f933da74b7eee804c6
Gong, Jing <jingong@amazon.com>
2015-03-24 18:41:19 -0700

    Add image save to server

--- public/javascripts/app/main.js
+++ public/javascripts/app/main.js
@@ -1,23 +1,4 @@
 define(["require", "jquery", "react", "feedback", "html2canvas", "init"], function(require, $, React, feedback, html2canvas, init) {
-
-	//the jquery.alpha.js and jquery.beta.js plugins have been loaded.
-	// tutorial1-raw.js
-	var CommentBox = React.createClass({
-		displayName: 'CommentBox',
-		render: function() {
-			return (
-				React.createElement('div', {
-						className: "commentBox"
-					},
-					"Hello, world! I am a CommentBox."
-				)
-			);
-		}
-	});
-	React.render(
-		React.createElement(CommentBox, null),
-		document.getElementsByTagName('feedback')[0]
-	);
 	Feedback({
 		h2cPath: html2canvas,
 		url: 'http://jingong.desktop.amazon.com:3001/feedback'
